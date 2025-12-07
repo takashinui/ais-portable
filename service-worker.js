9c6560f311c81082f7eb3cd2b481c0a4326714f4
takashinui <inui.takashi@inui.co.jp>
2025-12-07 18:31:58 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "inui-ship-viewer-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./register-sw.js",
+  "./manifest.webmanifest"
+  // アイコンを追加したらここにも入れてよい
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
