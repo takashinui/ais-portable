5d85516b526bbd3cc739a034d092c212fd546c3c
takashinui <inui.takashi@inui.co.jp>
2025-12-07 18:31:08 +0900

    Create register-sw.js

---
+++ register-sw.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .catch((err) => {
+        console.error("ServiceWorker registration failed:", err);
+      });
+  });
+}
